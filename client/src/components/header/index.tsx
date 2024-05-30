94c6e6818a5a56fe65c6e78f408992143129e01d
duong <duongtde170786@fpt.edu.vn>
2024-05-30 13:13:20 +0700

    test

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <></>
+  <div className=""></div>
     // <header className="w-full h-10 fixed top-0 left-0 bg-black text-white p-4 z-50 flex flex-row">
     //   <div className="self-center">
     //     <h1 className="text-md">My Header</h1>
