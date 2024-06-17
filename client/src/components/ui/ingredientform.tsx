af9fda3d9287fb4653a8e6e772888fca2736f49a
ngdth <thienndde170807@fpt.edu.vn>
2024-06-17 16:01:29 +0700

    recipe form

---
+++ client/src/components/ui/ingredientform.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+
+interface Ingredient {
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
+interface IngredientFormProps {
+  ingredients: Ingredient[];
+  setIngredients: (ingredients: Ingredient[]) => void;
+}
+
+const IngredientForm: React.FC<IngredientFormProps> = ({ ingredients, setIngredients }) => {
+  const handleAddIngredient = () => {
+      setIngredients([...ingredients, { name: '', quantity: 0, unit: '' }]);
+  };
+
+  const handleIngredientChange = (index: number, event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      const { name, value } = event.target;
+      const newIngredients = ingredients.slice();
+      newIngredients[index] = { ...newIngredients[index], [name]: value };
+      setIngredients(newIngredients);
+  };
+
+  const handleRemoveIngredient = () => {
+    if (ingredients.length > 1) {
+      setIngredients(ingredients.slice(0, ingredients.length - 1));
+    }
+  };
+
+  return (
+    <div>
+      {ingredients.map((ingredient, index) => (
+        <div key={index}>
+          <label>Nguyên liệu {index + 1}: </label>
+          <input
+            type="text"
+            name="name"
+            value={ingredient.name}
+            onChange={(e) => handleIngredientChange(index, e)}
+            className="border border-gray-300 p-2 mr-2 rounded-lg w-[30%]"
+            placeholder={`Input ${index + 1}`}
+          />
+          <input
+            type="number"
+            name="quantity"
+            value={ingredient.quantity}
+            onChange={(e) => handleIngredientChange(index, e)}
+            className="border border-gray-300 p-2 mr-2 rounded-lg w-[30%]"
+            placeholder={`Input ${index + 1}`}
+          />
+          <select
+            name="unit"
+            value={ingredient.unit}
+            onChange={(e) => handleIngredientChange(index, e)}
+            className="border border-gray-300 p-2 rounded-lg w-[26%]"
+          >
+            <option value="">Chọn đơn vị</option>
+            <option value="ingredientA">gram</option>
+            <option value="ingredientB">kilogram</option>
+            <option value="ingredientC">củ</option>
+          </select>
+        </div>
+      ))}
+      <button
+        type="button"
+        onClick={handleAddIngredient}
+        className="bg-blue-500 text-white p-2 rounded-lg mr-2 mt-2 mb-4"
+      >
+        Add Input
+      </button>
+      <button
+        type="button"
+        onClick={handleRemoveIngredient}
+        className="bg-red-500 text-white p-2 rounded-lg"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default IngredientForm;
