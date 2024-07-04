15a5904719b475e35fcd9d12c163dd633bfae1db
ngdth <thienndde170807@fpt.edu.vn>
2024-07-04 22:01:17 +0700

    update

--- client/src/app/user/page.tsx
+++
@@ -1,9 +0,0 @@
-// export default function User() {
-//   return <p>User Page</p>;
-// }
-
-import UserProfile from './component/profile/UserProfile';
-
-export default function User() {
-    return <UserProfile />;
-};
