df1a2392eb937bcebeeafe3b6d8014c8619cef2d
bluefishhh93 <xuanthanha03@gmail.com>
2024-07-07 10:38:52 +0700

    update upload

--- client/src/app/product/component/ProductComment/CommentList.tsx
+++ client/src/app/product/component/ProductComment/CommentList.tsx
@@ -9,9 +9,12 @@ interface CommentListProps {
 
 export const CommentList: React.FC<CommentListProps> = ({ comments, onImageClick }) => {
   return (
-    <div className="space-y-4">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} onImageClick={onImageClick} />
+    <div className="space-y-6">
+      {comments.map((comment, index) => (
+        <React.Fragment key={comment.id}>
+          {index > 0 && <hr className="border-gray-200" />}
+          <CommentItem comment={comment} onImageClick={onImageClick} />
+        </React.Fragment>
       ))}
     </div>
   );
